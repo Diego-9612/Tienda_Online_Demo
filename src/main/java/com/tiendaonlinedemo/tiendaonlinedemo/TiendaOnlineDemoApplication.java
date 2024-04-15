3ad77097d5368b24d12e169b0577ddd956361b74
Diego Fernando Guerrero <diego.guerrero9612@gmail.com>
2024-04-14 21:48:01 -0500

    Se agrega el proyecto creado con Spring Boot

---
+++ src/main/java/com/tiendaonlinedemo/tiendaonlinedemo/TiendaOnlineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaonlinedemo.tiendaonlinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaOnlineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaOnlineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaonlinedemo/tiendaonlinedemo/TiendaOnlineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaonlinedemo.tiendaonlinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaOnlineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
